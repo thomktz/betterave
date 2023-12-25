ae98aed8868e9a42f235f94f8c78618a3c6ca9ac
emanieluu <emanieluu@gmail.com>
2023-12-25 23:41:45 +0100

    Formatting grades table

--- frontend/src/views/controls/StudentGrades.vue
+++ frontend/src/views/controls/StudentGrades.vue
@@ -1,26 +1,28 @@
 <template>
-  <v-container class="fill-height">
-    <v-row>
-      <v-col v-for="enrolledClass in classes" :key="enrolledClass.class_id" cols="12">
-        <v-card>
-          <v-card-title>{{ enrolledClass.class_name }}</v-card-title>
-          <v-card-subtitle>ECTS: {{ enrolledClass.class_ects }}</v-card-subtitle>
-          <v-card-text>
-            <strong>Grades:</strong>
-            <v-list>
-              <v-list-item v-for="grade in gradesByClassId[enrolledClass.class_id]" :key="grade.grade_id">
-                {{ grade.grade }}
-              </v-list-item>
-            </v-list>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-container class="main-container"> <!-- Ajout de la classe text-center pour centrer -->
+    <!-- Grades Table -->
+    <v-simple-table class="grades-table mx-auto" dense>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="font-weight-bold text-left">Class Name</th>
+            <th class="font-weight-bold text-center">ECTS</th>
+            <th class="font-weight-bold text-center">Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in allGrades" :key="index">
+            <td>{{ item.class_name }}</td>
+            <td class="text-center">{{ item.class_ects }}</td>
+            <td class="text-center">{{ item.grades[0] ? item.grades[0].grade : 'No grade available' }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
   </v-container>
 </template>
-
 <script>
-import { apiClient, toast } from "@/apiConfig";
+import { apiClient } from "@/apiConfig";
 
 export default {
   data() {
@@ -32,7 +34,7 @@ export default {
       name: "",
       surname: "",
       level: null,
-      gradesByClassId: {}, // New object to store grades by class_id
+      allGrades: [], // New array to store all grades for display
     };
   },
   async mounted() {
@@ -52,32 +54,18 @@ export default {
     }
   },
   methods: {
-    async fetchClasses() {
-      try {
-        const response = await apiClient.get(`/classes/level/${this.level}`);
-        const allAvailableClasses = response.data;
-
-        this.availableClasses = allAvailableClasses.filter(
-          (availableClass) =>
-            !this.classes.some(
-              (enrolledClass) =>
-                enrolledClass.class_id === availableClass.class_id,
-            ),
-        );
-      } catch (error) {
-        console.error("Error fetching classes:", error);
-      }
-    },
     async fetchGradesForClasses() {
       try {
         for (const enrolledClass of this.classes) {
           const classId = enrolledClass.class_id;
           const response = await apiClient.get(`/users/${classId}/gradesbis/${this.user_id}`);
           const grades = response.data;
-          this.gradesByClassId = {
-            ...this.gradesByClassId,
-            [classId]: grades,
-          };
+
+          this.allGrades.push({
+            class_name: enrolledClass.class_name,
+            class_ects: enrolledClass.class_ects,
+            grades: grades,
+          });
         }
       } catch (error) {
         console.error("Error fetching grades for classes:", error);
@@ -88,5 +76,39 @@ export default {
 </script>
 
 <style scoped>
-/* Ajoutez des styles si nécessaire */
+.main-container {
+  background-color: var(--secondary-color);
+  color: var(--primary-text-color);
+  padding: 40px;
+  margin-top: 20px;
+  border-radius: 15px;
+}
+
+.grades-table {
+  width: 800px; /* Augmentation de la largeur */
+  margin-top: 20px;
+  border-collapse: collapse; /* Ajout pour afficher les bordures */
+}
+
+/* Augmentation de la largeur de la première colonne */.grades-table th:first-child,
+.grades-table td:first-child {
+  width: 800px; /* Ajustez la largeur de la première colonne selon vos besoins */
+  white-space: nowrap; /* Empêche le retour à la ligne dans la première colonne */
+}
+
+
+.font-weight-bold {
+  font-weight: bold;
+}
+
+.grades-table td,
+.grades-table th {
+  padding: 8px;
+  border: 1px solid white; /* Remplacez 'white' par '#ffffff' si vous préférez utiliser la notation hexadécimale */
+}
+
+/* Ajout d'un fond coloré pour améliorer la visibilité des cellules */
+.grades-table td {
+  background-color: var(--secondary-color);
+}
 </style>
