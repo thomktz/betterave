dd52b049848690305fdaf509994bf943cf7dfda1
elenalmg <elena.loumagne@gmail.com>
2024-01-05 00:34:13 +0100

    delete bad import

--- backend/tests/test_students_operations.py
+++ backend/tests/test_students_operations.py
@@ -1,7 +1,7 @@
 import pytest
-from backend.app.models.class_group import ClassGroup
-from backend.app.models.user import User
-from backend.app.models.class_ import Class
+from app.models.class_group import ClassGroup
+from app.models.user import User
+from app.models.class_ import Class
 from app.models import UserType, UserLevel
 from app.operations.class_operations import add_class
 from app.operations.class_group_operations import add_class_group, delete_class_group
