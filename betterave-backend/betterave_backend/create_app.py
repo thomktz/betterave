2a4268e4b64a312a3d09150107871ba9d303d284
cdechaux <claire.dechaux@ensae.fr>
2024-03-06 16:30:31 +0100

    modif create_app.py with api notification

--- betterave-backend/betterave_backend/create_app.py
+++ betterave-backend/betterave_backend/create_app.py
@@ -17,6 +17,7 @@ from betterave_backend.app.api import (
     class_groups_ns,
     user_class_groups_ns,
     events_ns,
+    notifications_ns,
 )
 
 
@@ -86,6 +87,7 @@ def create_app(db_test_path: Optional[str] = None) -> Flask:
     api.add_namespace(class_groups_ns, path="/class_groups")
     api.add_namespace(user_class_groups_ns, path="/user_class_groups")
     api.add_namespace(events_ns, path="/events")
+    api.add_namespace(notifications_ns, path="/notifications")
 
     # Load/create the database
     with app.app_context():
